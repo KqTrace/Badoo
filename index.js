b11150f58766f4eb72fdbe5886e1753afec0c5c6
Kaique <souzakaique2@gmail.com>
2020-05-20 21:37:48 -0300

    Adicao do heading/front do maincontent

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(React.createElement(
+  "h1",
+  null,
+  "Ol\xE1!"
+), document.getElementById('maincont'));
---
+++ src/LikeButton.js
@@ -0,0 +1,23 @@
+/*
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return (
+      <button onClick={() => this.setState({ liked: true }) }>
+        Like
+      </button>
+    );
+  }
+}
+
+let domContainer = document.querySelector('#maincont');
+ReactDOM.render(<LikeButton />, domContainer);
+*/
